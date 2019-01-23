c6e32c17fe29a80171664355df926f4c879ff0a8
dsukesh <46971114+dsukesh@users.noreply.github.com>
2019-01-23 22:51:15 +0530

    Add files via upload

---
+++ MAP_FOLLOWER_GIT/PYTHON/bluetooth.py
@@ -0,0 +1,38 @@
+import serial
+#import time
+#import math
+
+port = "COM12"
+bluetooth = serial.Serial(port,9600);
+print('CONNECTED');
+
+count = 0
+
+while(count<len(inst)):
+     
+    #SENDING MOTOR NUMBER
+   #print('###')
+   #bluetooth.read()
+   print('+++')
+   bluetooth.write(str(int(inst[count][0])))
+   #print(int(str(inst[count][0])))
+   #x = bluetooth.read()
+   #print x
+   if(bluetooth.read()=='2'):
+       print('ACKNOWLEDGED :)')
+   
+    #SENDING DISTANCE IN PIXELS
+    
+   bluetooth.write(str((int(inst[count][1]))/10))  
+   if(bluetooth.read()=='3'):
+       print('ACKNOWLEDGED :)')
+   
+    #SENDING ANGLE IN DEGREES [0,360)
+   
+   bluetooth.write(str(int(inst[count][2])))
+   if(bluetooth.read()=='1'):
+       print('ACKNOWLEDGED :)')
+    
+    #COUNTER INCREMENTATION
+   count += 1
+   bluetooth.read()
